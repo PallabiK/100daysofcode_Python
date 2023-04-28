db1621b7e9b7f108c9c103823a3544e1e651362a
PallabiK <pallabikundu95@gmail.com>
2023-04-27 21:22:07 -0500

    Day 5

--- password_generator.py
+++ password_generator.py
@@ -11,16 +11,15 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+password = []
 for i in range(0, nr_letters):
-  password+=random.choice(letters)
+  password.append(random.choice(letters))
 for j in range(0, nr_symbols):
-  password+=random.choice(symbols)
+  password.append(random.choice(symbols))
 for k in range(0, nr_numbers):
-  password+=random.choice(numbers)
+  password.append(random.choice(numbers))
 
-shuffled_password=list(password)
-random.shuffle(shuffled_password)
-shuffled_password=''.join(shuffled_password)
+random.shuffle(password)
+password=''.join(password)
 
 print(f'Your shuffled password can be {shuffled_password}.')
