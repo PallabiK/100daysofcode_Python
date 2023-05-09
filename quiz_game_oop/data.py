e36b34301876abd7926454db01b75140bbd9d766
PallabiK <pallabikundu95@gmail.com>
2023-05-09 11:01:44 -0500

    Day 17

---
+++ quiz_game_oop/data.py
@@ -0,0 +1,29 @@
+question_data = [
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "&quot;Typewriter&quot; is the longest word that can be typed"
+                 "using only the first row on a QWERTY keyboard.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "Coca-Cola&#039;s original colour was green.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+                                      "question": "The vapor produced by e-cigarettes is actually water.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "There are 86400 seconds in a day.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "An eggplant is a vegetable.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "Cucumbers are usually more than 90% water.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+                                       "question": "The average woman is 5 inches \/ 13 centimeters "
+                                                   "shorter than the average man.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "Kissing someone for one minute burns about 2 calories.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+                                       "question": "Fast food restaurant chains Carl&#039;s Jr. "
+                                                   "and Hardee&#039;s are owned by the same company.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "Instant mashed potatoes were invented by Canadian Edward Asselbergs in 1962.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}]
