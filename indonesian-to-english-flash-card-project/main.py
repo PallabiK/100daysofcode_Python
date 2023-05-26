82c96131b63d91d957df26ca81003246a1047fc2
PallabiK <pallabikundu95@gmail.com>
2023-05-26 11:54:18 -0500

    Day 31

--- indonesian-to-english-flash-card-project/main.py
+++ indonesian-to-english-flash-card-project/main.py
@@ -10,9 +10,9 @@ current_card = {}
 
 #-------Data File -----#
 try:
-    data=pandas.read_csv("data/words_to_learn.csv")
+    data = pandas.read_csv("data/words_to_learn.csv")
 except FileNotFoundError:
-    original_data = pandas.read_csv("data/french_words.csv")
+    original_data = pandas.read_csv("data/indonesian_words.csv")
     to_learn = original_data.to_dict(orient="records")
 else:
     to_learn = data.to_dict(orient="records")
