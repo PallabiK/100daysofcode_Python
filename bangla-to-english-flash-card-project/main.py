b93116e742184150f78caf655d974dde57eeb244
PallabiK <pallabikundu95@gmail.com>
2023-06-03 16:19:09 -0500

    Bangla-to-English

--- bangla-to-english-flash-card-project/main.py
+++ bangla-to-english-flash-card-project/main.py
@@ -4,7 +4,6 @@ from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT = "Ariel"
-#TIMER = 3000
 
 current_card = {}
 
@@ -20,13 +19,11 @@ else:
 
 #-------Functions------#
 def next_card():
-    global current_card #, flip_timer
-    #window.after_cancel(flip_timer)
+    global current_card
     current_card = choice(to_learn)
     canvas.itemconfig(card_background, image=card_front_img)
     canvas.itemconfig(card_title, text="Bangla", fill="black")
     canvas.itemconfig(card_word, text=current_card["Bangla"], fill="black")
-    #flip_timer = window.after(TIMER, func=flip_card)
 
 def is_known():
     to_learn.remove(current_card)
@@ -45,6 +42,15 @@ def flip_card():
         canvas.itemconfig(card_title, text="Bangla", fill="black")
         canvas.itemconfig(card_word, text=current_card["Bangla"], fill="black")
 
+def alphabet_window():
+    alphabet_img = PhotoImage(file="images/bangla_alphabet.png")
+    new_window = Toplevel()
+    new_window.title("Bangla Letters")
+    new_canvas = Canvas(new_window, width=390, height=580)
+    new_canvas.create_image(0, 0, image=alphabet_img, anchor=NW)
+    new_canvas.grid(column=0, row=0)
+    new_canvas.alphabet_img = alphabet_img
+
 
 #-------UI Setup ------#
 window = Tk()
@@ -58,15 +64,12 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 card_front_img=PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
-canvas.grid(column=0, row=0, columnspan=3)
+canvas.grid(column=0, row=0, columnspan=4)
 card_title = canvas.create_text(400, 150, text="Title", fill="black", font=(FONT, 40, "italic"))
 card_word = canvas.create_text(400, 263, text="Word", font=(FONT, 60, "bold"))
 
 
-
-
-
-#canvas with right and wrong buttons
+#buttons
 unknown_img = PhotoImage(file="images/wrong.png")
 unknown_button = Button(image=unknown_img, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=1)
@@ -77,15 +80,13 @@ flip_button.config(padx=10, pady=10)
 
 known_img = PhotoImage(file="images/right.png")
 known_button = Button(image=known_img, highlightthickness=0, command=is_known)
-known_button.grid(column=2, row=1)
+known_button.grid(column=3, row=1)
+
+
+alphabet_button = Button(text="Bangla\nLetters", font=(FONT, 20, "bold"), bg=BACKGROUND_COLOR, command=alphabet_window)
+alphabet_button.grid(column=2, row=1)
 
 next_card()
-
-
-
-
-
-
 
 
 window.mainloop()
