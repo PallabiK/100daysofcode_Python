173ea4296cb7af8a2afdd32a0d367084eeb80162
PallabiK <pallabikundu95@gmail.com>
2023-06-12 21:18:47 -0500

    Day 49

--- day_49/main.py
+++ day_49/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 ACCOUNT_EMAIL = YOUR LOGIN EMAIL
@@ -15,7 +16,7 @@ driver.get(url)
 sign_in_button = driver.find_element(By.LINK_TEXT, "Sign in")
 sign_in_button.click()
 
-time.sleep(5)
+time.sleep(2)
 
 email_field = driver.find_element(By.ID, "username")
 email_field.send_keys(ACCOUNT_EMAIL)
@@ -24,14 +25,58 @@ password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
 time.sleep(5)
-apply_button = driver.find_element(By.CSS_SELECTOR, ".artdeco-button__text button")
-apply_button.click()
+# apply_button = driver.find_element(By.CSS_SELECTOR, ".artdeco-button__text button")
+# apply_button.click()
+#
+# time.sleep(5)
+# phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
+# if phone.text == "":
+#     phone.send_keys(PHONE)
+#
+# submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
+# submit_button.click()
+
+all_listings = driver.find_elements_by_css_selector(".job-card-container--clickable")
+
+for listing in all_listings:
+    print("called")
+    listing.click()
+    time.sleep(2)
+
+    try:
+        apply_button = driver.find_element(By.CSS_SELECTOR, ".jobs-s-apply button")
+        apply_button.click()
+        time.sleep(5)
+
+        # If phone field is empty, then fill your phone number.
+        phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
+        if phone.text == "":
+            phone.send_keys(PHONE)
+
+        submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
+
+        # If the submit_button is a "Next" button, then this is a multi-step application, so skip.
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+            close_button.click()
+            time.sleep(2)
+            discard_button = driver.find_elements(By.CLASS_NAME, "artdeco-modal__confirm-dialog-btn")[1]
+            discard_button.click()
+            print("Complex application, skipped.")
+            continue
+        else:
+            submit_button.click()
+
+        # Once application completed, close the pop-up window.
+        time.sleep(2)
+        close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+        close_button.click()
+
+    # If already applied to job or job is no longer accepting applications, then skip.
+    except NoSuchElementException:
+        print("No application button, skipped.")
+        continue
 
 time.sleep(5)
-phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
-if phone.text == "":
-    phone.send_keys(PHONE)
-
-submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
-submit_button.click()
+driver.quit()
 
