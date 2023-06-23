45703778b191dfbcdfe35c9793ba81db9e06f4bd
PallabiK <pallabikundu95@gmail.com>
2023-06-23 16:07:40 -0500

    Day 63

---
+++ day_63_sql/main.py
@@ -0,0 +1,50 @@
+import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# db = sqlite3.connect("books-collection.db")
+#
+# cursor = db.cursor()
+#
+# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) "
+# #                "NOT NULL, rating FLOAT NOT NULL)")
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+
+
+app = Flask(__name__)
+
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __init__(self, id, title, author, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.rating = rating
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+with app.app_context():
+    db.create_all()
+    book = Book(2, 'Harry Potter', 'J. K. Rowling', '9.3')
+    db.session.add(book)
+    db.session.commit()
+
+if __name__ == "__main__":
+    app.run(debug=True)
